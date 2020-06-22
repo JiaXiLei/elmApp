5b0d7237d5d2c9f67a879e4da7796b5551da6eb2
hyypro <3126924360@qq.com>
2020-06-22 20:45:11 +0800

    'ss'

--- src/routers/routes.js
+++ src/routers/routes.js
@@ -7,13 +7,6 @@ const Search = loadable(() => import('@/pages/search'))   //搜索页面
 const Order = loadable(() => import('@/pages/order'))   //订单页面
 const User = loadable(() => import('@/pages/user'))   //用户页面
 
-// import Login from '@/pages/login'
-// import Home from '@/pages/home'
-// import HomeCount from '@/pages/home/count'
-// import Search from '@/pages/search'
-// import Order from '@/pages/order'
-// import User from '@/pages/user'
-
 export {
     Login,
     Home,
